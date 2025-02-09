877f13da8723f599338b795d4c915f1bc84569a8
Chandrashekhar Nandanwar <shekhar15jan@gmail.com>
2025-02-09 21:01:12 +0530

    Angular Forms

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
-
-  constructor() { }
+  form1!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form1 = this.formBuilder.group({
+      fullName: ['',[Validators.required]],
+      email: ['',[Validators.required, 
+        Validators.pattern('^[a-z0-9,_%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+      message: ['',[Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(50)]]
+    });
   }
 
 }
